af5038e94e925f94e23dc1d2f76673590d63cc30
yliuosu <yliuosu@gmail.com>
2017-07-19 23:08:15 -0500

    feat: Implement the prediction function.

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -159,6 +159,43 @@ void UKF::Prediction(double delta_t) {
   Complete this function! Estimate the object's location. Modify the state
   vector, x_. Predict sigma points, the state, and the state covariance matrix.
   */
+  
+  //create augmented mean vector
+  VectorXd x_aug = VectorXd(n_aug_);
+
+  //create augmented state covariance
+  MatrixXd P_aug = MatrixXd(n_aug_, n_aug_);
+
+  //create sigma point matrix
+  MatrixXd Xsig_aug = MatrixXd(n_aug_, 2 * n_aug_ + 1);
+
+  //create augmented mean state
+  x_aug.head(n_x_) = x_;
+  x_aug(5) = 0;
+  x_aug(6) = 0;
+  
+  //  cout << "Dt" << endl << delta_t << endl;
+  //  cout << "x_aug" << endl << x_aug << endl;
+  
+  P_aug.fill(0.0);
+  //create augmented covariance matrix
+  P_aug.topLeftCorner( n_x_, n_x_ ) = P_;
+  P_aug(5,5) = std_a_ * std_a_;
+  P_aug(6,6) = std_yawdd_ * std_yawdd_;
+
+  //create square root matrix
+  MatrixXd A = P_aug.llt().matrixL();
+  MatrixXd root =  sqrt(lambda_ + n_aug_) * A;
+    
+  //create augmented sigma points
+  Xsig_aug.col(0) = x_aug;
+  for(int i = 0; i < n_aug_; i++){
+      Xsig_aug.col(1 + i) = x_aug + root.col(i);
+      Xsig_aug.col(1 + n_aug_ + i) = x_aug - root.col(i);
+//    Xsig_aug(3,1 + i) = Normalize(Xsig_aug(3,1 + i));
+//    Xsig_aug(3,1 + n_aug_ + i) = Normalize(Xsig_aug(3,1 + i));
+  }
+
 }
 
 /**
