15670be23dfa7fcda8573e0f83cf6d21672357df
yliuosu <yliuosu@gmail.com>
2017-07-23 23:45:51 -0500

    feat: Add the normalization function.

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -398,3 +398,10 @@ void UKF::UpdateRadar(MeasurementPackage meas_package) {
   
   return;
 }
+
+double UKF::Normalize(double x) {
+  while(x < -M_PI) x += 2 * M_PI;
+  while(x >  M_PI) x -= 2 * M_PI;
+  
+  return x;
+}
